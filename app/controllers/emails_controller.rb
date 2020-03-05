db4abe5274b92adbb950a5cbe66636d4972af8ef
amine91480 <amine91480@hotmail.fr>
2020-03-05 20:48:22 +0100

    2.4. Deuxième niveau d'AJAX : afficher un email -> Bientôt réussi

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -3,6 +3,14 @@ class EmailsController < ApplicationController
     @allMail = Email.all
   end
 
+  def show
+    @email = Email.find(params[:id])
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js {  }
+      end
+  end
+
   def create
     @email = Email.create(object: Faker::TvShows::SiliconValley.company,
                  body: Faker::TvShows::SiliconValley.quote,
